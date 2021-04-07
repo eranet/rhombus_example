dcf99dc29f999d43aeb446af0f4dc30b2fe7a179
l1va <lev_matematik@tut.by>
2021-04-07 16:50:59 +0300

    update version

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/l1va/rhombus_example
 
 go 1.12
 
-require github.com/l1va/rhombus v0.0.0-20190701184750-7d8408117294 // indirect
+require github.com/eranet/rhombus v0.0.2
--- launch_world.sh
+++ launch_world.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./server/gnatsd  &
+./server/gnatsd -DV &
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:./plugins
-gazebo manipulator.world --verbose
+gazebo manipulator.world --verbose &
+go run control/commands.go
